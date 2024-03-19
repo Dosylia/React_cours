f3aad935e304a9b873386d055740dec322a2ceab
Dosylia <n7xemma@gmail.com>
2024-03-19 11:06:42 +0100

    Ajout d'un pannier avec son total

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-function Header() 
+function Banner() 
 {
-    return(<h1>La maison jungle</h1>);
+    const title = "La maison jungle";
+    return(<h1>{title.toUpperCase()}</h1>);
 }
 
 function Cart() 
 {
+    let prixMonstera = 8;
+    let prixLierre = 10;
+    let prixBouquetDeFleurs = 15;
     return (<div>
-        <p>Monstera</p>
-        <p>Lierre</p>
-        <p>Bouquet de fleur</p>
+        <h2>Panier</h2>
+        <ul>
+            <li>Monstera : {prixMonstera} €</li>
+            <li>Lierre : {prixLierre} €</li>
+            <li>Bouquet de fleurs : {prixBouquetDeFleurs} €</li>
+        </ul>
+        <p>Total : {prixMonstera + prixLierre + prixBouquetDeFleurs} €</p>
     </div>)
 }
 
-function Banner() 
-{
-    return (<div>
-        <Header />
-    </div>)
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+    <div>
         <Banner/>
+        <Cart />
+    </div>
 );
